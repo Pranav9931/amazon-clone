4e05dd418fb91b91479bf354c66791fc8e7f292b
Pranav Kumar <pranavsandilya786@gmail.com>
2022-01-02 12:23:03 +0530

    Simple App Wireframe

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBcX0w6k7vsowxVQkw1d-TeFOZcMbc34Xc",
+  authDomain: "clone-1ad21.firebaseapp.com",
+  projectId: "clone-1ad21",
+  storageBucket: "clone-1ad21.appspot.com",
+  messagingSenderId: "129196710874",
+  appId: "1:129196710874:web:4ff85e65a8405047f33ee3",
+  measurementId: "G-KRHVQHYTZT",
+};
